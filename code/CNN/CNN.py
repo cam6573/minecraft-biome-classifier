43e9661249bf603db6aee161d3f2373006669145
Sebastian Canakis <sac2693@g.rit.edu>
2026-03-25 13:49:44 -0400

    CNN, predict_an_image has been completed

---
+++ code/CNN/CNN.py
@@ -0,0 +1,72 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+
+IMG_HEIGHT = 128
+IMG_WIDTH = 128
+BATCH_SIZE = 32
+NUM_CLASSES = 6
+
+
+TRAIN_DIR = "data/processed/CNN/training"
+VALIDATION_DIR = "data/processed/CNN/validation"
+
+
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    TRAIN_DIR,
+    image_size = (IMG_HEIGHT, IMG_WIDTH),
+    batch_size=BATCH_SIZE
+)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+    VALIDATION_DIR,
+    image_size = (IMG_HEIGHT, IMG_WIDTH),
+    batch_size=BATCH_SIZE
+)
+
+class_names = train_ds.class_names
+print(f"Classes: {class_names}")
+
+AUTOTUNE = tf.data.AUTOTUNE
+train_ds = train_ds.shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.prefetch(buffer_size=AUTOTUNE)
+
+model = keras.Sequential([
+    layers.Rescaling(1./255, input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)),
+
+    layers.Conv2D(32, (3, 3), activation='relu'),
+    layers.MaxPooling2D(),
+
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D(),
+
+    layers.Conv2D(128, (3, 3), activation='relu'),
+    layers.MaxPooling2D(),
+
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dropout(0.3),
+    layers.Dense(NUM_CLASSES, activation='softmax')
+])
+
+# Compile
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+# Show model structure
+model.summary()
+
+# Train
+history = model.fit(
+    train_ds,
+    validation_data=val_ds,
+    epochs=10
+)
+
+# Save model
+model.save("minecraft_biome_cnn.keras")
